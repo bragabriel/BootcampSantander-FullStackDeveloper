df69022e3404b965b3c76c1be90a97072d6bd432
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-28 11:16:39 -0300

    add file

---
+++ JavaScript/tipos-variaveis/num.js
@@ -0,0 +1,20 @@
+let num = 100
+
+console.log(num*5)
+console.log(num%2)
+
+
+console.log(typeof(Math))
+console.log(Math.PI)
+
+
+//Arredonda para baixo
+console.log(Math.floor(5/3))
+
+//Arredonda para cima
+console.log(Math.ceil(5/3))
+
+let percent = 10
+console.log(percent + "%")
+
+console.log(typeof(percent.toString()))
