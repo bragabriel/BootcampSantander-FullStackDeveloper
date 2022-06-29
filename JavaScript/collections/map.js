9fddcc2f5f4c444dddcd7447b53e590e29774e16
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-29 14:28:07 -0300

    changed folder

--- JavaScript/collections/map.js
+++ JavaScript/collections/map.js
@@ -1,4 +1,12 @@
+/*
+ Coleção de arrays no formato [chave, valor]
+ Pode set iterado por um loop for...of
+ */
+
 const myMap = new Map()
+
+
+/* Métodos: */
 
 myMap.set('apple', 'fruit');
 // Map(1) {"apple" => "fruit"}
