66f47b5aa5fb0e327bbe28d4315af56d82bf4686
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-29 11:58:18 -0300

    Create arrowFunc.js

---
+++ JavaScript/arrow-function/arrowFunc.js
@@ -0,0 +1,12 @@
+//funcao normal
+const helloWorld = function(){
+    return "Hello World"
+}
+
+//arrow function
+const helloWorldArrow = () =>{
+    return "Hello World"
+}
+
+//arrow function simplificada
+const helloWorldArrow2 = () => "Hello World"
