45856b6ecc89e5b37df42a18a7ae80b1dbd6a3e2
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-30 17:46:54 -0300

    Update map.js

--- JavaScript/map-filter-reduce/Atividades/map.js
+++ JavaScript/map-filter-reduce/Atividades/map.js
@@ -1,3 +1,7 @@
+/* Map com THIS */
+console.log('')
+console.log('Map com THIS')
+
 const maca = {
     value: 2,
 }
@@ -18,3 +22,22 @@ numeros = [1, 2];
 console.log('Resultado quando this = maçã ' + mapComThis(numeros, maca));
 
 console.log('Resultado quando this = laranja ' + mapComThis(numeros, laranja));
+
+/* *** */
+
+
+/* Map sem THIS */
+console.log('')
+console.log('Map sem THIS')
+
+function mapSemThis(arr){
+    return arr.map(function(item){
+        return item * 2;
+    })
+}
+
+const nums = [2, 4, 6, 8, 10];
+
+console.log('Array com map: ' + mapSemThis(nums))
+console.log('Array original: ' + nums)
+/* *** */
