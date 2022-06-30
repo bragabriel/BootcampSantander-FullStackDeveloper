027527bfee4e407a77a09ade626b5e529c64a1d9
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-30 17:43:46 -0300

    Create reduce.js

---
+++ JavaScript/map-filter-reduce/reduce.js
@@ -0,0 +1,9 @@
+/* Executa uma função em todos os elementos do array e retorna um valor único */
+
+const callbackFn = function(accumulator, currentValue, index, array){
+//                  Accumulator/prevValue: acumulador de todas as chamadas de callbackFn
+//                  currentValue: elemento atual sendo acessado pela função
+    //do something
+}
+
+array.reduce(callbackFn, initialValue)
