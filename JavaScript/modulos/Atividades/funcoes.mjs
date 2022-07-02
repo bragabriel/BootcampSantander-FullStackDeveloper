542ec36930a97b4be42b21cfb7e654f377f5e570
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-02 13:59:53 -0300

    add files

---
+++ JavaScript/modulos/Atividades/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`
+}
+
+function mostraCidade(nome, Cidade){
+    return `A Cidade de ${nome} é ${Cidade}`
+}
+
+function mostraHobby(nome, Hobby){
+    return `A Hobby de ${nome} é ${Hobby}`
+}
+
+export{
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+}
---
+++ JavaScript/modulos/Atividades/main.mjs
@@ -0,0 +1,7 @@
+//Importando as funções do arquivo: 'funcoes.mjs'
+
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gabriel", 20))
+console.log(mostraCidade("Gabriel", "Pirassununga"))
+console.log(mostraHobby("Gabriel", "Assistir séries"))
