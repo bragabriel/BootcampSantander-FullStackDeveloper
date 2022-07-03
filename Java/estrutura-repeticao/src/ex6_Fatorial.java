a23bc8c98a25d440a0d0448bcf22cc48dafdbdc7
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-03 17:15:48 -0300

    Create ex6_Fatorial.java

---
+++ Java/estrutura-repeticao/src/ex6_Fatorial.java
@@ -0,0 +1,12 @@
+
+public class ex6_Fatorial {
+
+	public static void main(String[] args) {
+		/* Faça um programa que calcule o fatorial de um número inteiro
+		 * fornecido pelo usuário
+		 * Ex: 5!=120
+		 * 
+		*/
+	}
+
+}
