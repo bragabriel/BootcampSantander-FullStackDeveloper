583fbfd63d1ff477f65a18a46c804b935ecbb736
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-05 11:59:08 -0300

    add files

---
+++ Java/collections/src/collections_framework/collections_introduction.java
@@ -0,0 +1,13 @@
+package collections_framework;
+
+public class collections_introduction {
+
+	public static void main(String[] args) {
+		/* Collection é um objeto que agrupa múltiplos elementos (variáveis primitivas ou objetos)
+		 * dentro de uma única unidade.
+		 * 
+		 * Serve para armazenar e processar conjuntos de dados de forma eficiente
+		 */
+	}
+
+}
---
+++ Java/collections/src/collections_framework/iteracao_metodos.java
@@ -0,0 +1,102 @@
+package collections_framework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class iteracao_metodos {
+
+	public static void main(String[] args) {
+		
+		/* ArrayList: deve ser usado onde mais operações de pesquisa são necessárias.
+		 * LinkedList: deve ser usado onde mais operações de inserção e exclusão são necessárias.*/
+	
+		System.out.println("\nCrie uma lista e adicione sete notas: ");
+		
+		List<Double> notas = new ArrayList<Double>();
+		notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+		
+		System.out.println(notas);
+		System.out.println(notas.toString());
+		
+		
+		System.out.println("\nExiba a posição da nota 5.0: " + notas.indexOf(5d));
+
+		
+	    System.out.println("\nAdicione na lista a nota 8.0 na posição 4: ");
+	    notas.add(4, 8d);
+	    System.out.println(notas);
+
+	    
+	    System.out.println("\nSubstitua a nota 5.0 pela nota 6.0: ");
+	    notas.set(notas.indexOf(5d), 6.0);
+	    System.out.println(notas);
+
+	    
+	    System.out.println("\nConfira se a nota 5.0 está na lista: " + notas.contains(5d));
+
+	    
+	    System.out.println("\nExiba todas as notas na ordem em que foram informados: ");
+	    for (Double nota : notas) System.out.print(nota + " ");
+
+	    
+	    System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+	    System.out.println(notas.toString());
+	    
+
+	    System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+	    
+	    System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+	        
+	    
+	    Iterator<Double> iterator = notas.iterator();
+	        
+	    Double soma = 0d;
+	        
+	    while(iterator.hasNext()){
+	    	Double next = iterator.next();
+	    	soma += next;
+	    }
+	    
+	    System.out.println("Exiba a soma dos valores: " + soma);
+
+	    
+	    System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+	    
+	    System.out.println("Remova a nota 0: ");
+	    notas.remove(0d);
+	    System.out.println(notas);
+
+	    
+	    System.out.println("Remova a nota da posição 0");
+	    notas.remove(0);
+	    System.out.println(notas);
+
+	    
+	    System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+	    Iterator<Double> iterator1 = notas.iterator();
+	    while(iterator1.hasNext()) {
+	    	Double next = iterator1.next();
+	    	if(next < 7) iterator1.remove();
+	    }
+	    
+	    System.out.println(notas);
+
+	    /*System.out.println("Apague toda a lista");
+	    notas.clear();
+	    System.out.println(notas);
+	    */
+	    System.out.println("Confira se a lista está vazia: " + notas.isEmpty()); 
+	}
+}
