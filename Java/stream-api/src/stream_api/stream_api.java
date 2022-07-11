92f433fbcc237b4f1b660b81ccea47d948c67d69
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-11 14:23:37 -0300

    add files

---
+++ Java/stream-api/src/stream_api/stream_api.java
@@ -0,0 +1,14 @@
+package stream_api;
+
+public class stream_api {
+
+	public static void main(String[] args) {
+		/*A Stream API traz uma nova opção para a manipulação de coleçoes em Java,
+		 * seguindo os princípios da programação funcional. Combinada com as expressões lambda,
+		 * ela proporciona uma forma diferente de lidar com conjuntos de elementos, oferecendo ao 
+		 * desenvovedor uma maneira simples e concisa de escrever código que resulta em facilidade de
+		 * manutenção e paralelização sem efeitos indesejados em tempo de execução*/
+
+	}
+
+}
