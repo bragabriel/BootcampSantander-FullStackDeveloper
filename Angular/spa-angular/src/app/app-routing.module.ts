6a95a2f6c7b3f3ef8a43f6712b11b7a6607d559a
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-16 20:49:21 -0300

    feat: data binding

--- Angular/spa-angular/src/app/app-routing.module.ts
+++ Angular/spa-angular/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
 import { HomeComponent } from './home/home.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
 const routes: Routes = [
+  //Home
   {
     path: '',
     component: HomeComponent
+  },
+  //Aluno
+  {
+    path: 'alunos',
+    component: AlunosComponent
+  },
+  //Professor
+  {
+    path: 'professores',
+    component: ProfessoresComponent
   }
 ];
 
