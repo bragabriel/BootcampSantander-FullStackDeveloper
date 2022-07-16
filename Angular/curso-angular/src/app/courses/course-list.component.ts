66dd986034ee9df4c6bf9a310749910e85537334
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-16 16:44:31 -0300

    final commit

--- Angular/curso-angular/src/app/courses/course-list.component.ts
+++ Angular/curso-angular/src/app/courses/course-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
 import { CourseService } from './course-service';
-import { Inject } from '@angular/core';
 
 @Component({
     templateUrl: './course-list.component.html'
