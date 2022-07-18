cae2913e14d571313ac126c92e42e8d96d869184
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-18 15:41:05 -0300

    feat: diretivas de atributo e estruturais

--- Angular/curso_ambiente-angular/src/app/app.component.ts
+++ Angular/curso_ambiente-angular/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  count = 0;
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    let interval = setInterval(() => {
+      this.count++;
+      
+      if(this.count === 10){
+        clearInterval(interval);
+      }
+
+    }, 1000)
+  }
+
 }
