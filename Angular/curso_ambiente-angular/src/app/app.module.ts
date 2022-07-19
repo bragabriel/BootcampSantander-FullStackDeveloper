49fc3b16a234d519cccea68e467eacd832c29385
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-19 14:48:24 -0300

    folder changed

--- Angular/curso_ambiente-angular/src/app/app.module.ts
+++ Angular/curso_ambiente-angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MarcaTextoDirective } from './directives/marca-texto.directive';
+import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
 
 @NgModule({
   declarations: [
