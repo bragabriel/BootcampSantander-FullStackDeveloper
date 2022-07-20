a3130edbe318435326c5b4e261eddf2cffa852c2
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-20 20:31:44 -0300

    feat: users-list component

--- Angular/crud-angular/src/app/app-routing.module.ts
+++ Angular/crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UsersListComponent} /* Para o caminho vazio, eu quero renderizar o componente... */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/crud-angular/src/app/app.module.ts
+++ Angular/crud-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
