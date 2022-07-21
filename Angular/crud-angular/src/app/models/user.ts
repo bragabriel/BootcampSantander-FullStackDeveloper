84874f938f7c8fdbecb50dbcbb0fdfad83e8cf04
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-21 13:18:58 -0300

    feat: interface service-user

---
+++ Angular/crud-angular/src/app/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    id?: number, 
+    nome: string,
+    sobrenome: string,
+    idade: number,
+    profissao: string
+}
+/* id?:   =  ? é para não ser obrigatório */
--- Angular/crud-angular/src/app/pages/users/users-list/users-list.component.ts
+++ Angular/crud-angular/src/app/pages/users/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  /* Definindo nosso array para receber os dados, do tipo User (dados que vem da API) */
+  users: Array<User> = [];
+
+  /* Injeção de dependencia do nosso UserService */
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
+  /* Método que vai na API, e traz os dados */
+  getUsers(): void{
+    /* Pegando a reposta do nosso serviço UserService */
+    this.userService.getUsers().subscribe(response => {
+    //                          subscribe retorna de CallBack o reponse da APi  
+      this.users = response
+    })
+  }
 }
