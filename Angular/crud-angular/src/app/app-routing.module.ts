722dc83c3705f2a12e251cf09b4a7486f0554d68
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-21 10:52:46 -0300

    feat: user-form component

--- Angular/crud-angular/src/app/app-routing.module.ts
+++ Angular/crud-angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 const routes: Routes = [
-  {path: '', component: UsersListComponent} /* Para o caminho vazio, eu quero renderizar o componente... */
+  {path: '', component: UsersListComponent}, /* Para o caminho vazio, eu quero renderizar o componente... */
+  {path: 'form', component: UserFormComponent} /*Para o caminho '/form' eu quero renderizar o componente...*/
 ];
 
 @NgModule({
--- Angular/crud-angular/src/app/app.module.ts
+++ Angular/crud-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UsersListComponent
+    UsersListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
