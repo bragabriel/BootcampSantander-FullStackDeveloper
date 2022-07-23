cc6d7f52c6adcf20669d85fad32ac79d2d1b4456
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-22 21:20:15 -0300

    add: data-binding

--- Angular/crud-angular/src/app/pages/data-binding/data-binding.component.ts
+++ Angular/crud-angular/src/app/pages/data-binding/data-binding.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.css']
 })
-export class DataBindingComponent implements OnInit {
+export class DataBindingComponent implements OnInit { 
+
+  text = 'Interpolação -> Do componente para o D.O.M';
+  imgUrl = 'https://picsum.photos/300/300';
+  imgAlt = 'Essa é uma imagem';
+  buttonText = 'Criando atributo e atribuindo valor direto pelo componente =)';
+  textRed = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  retornaNome(){
+    return "OLAAAA da função"
+  }
+
+  clicouBtn(){
+    console.log('Clicou! =)')
+    this.text = 'Clicou no botãoooooo, iiiiihhhhhhhhhhhh'
+  }
+
+  clicouInput(value: any){
+    console.log('Clicou', value)
+  }
+
+  clicouClass(){
+    this.textRed = true
+  }
 }
