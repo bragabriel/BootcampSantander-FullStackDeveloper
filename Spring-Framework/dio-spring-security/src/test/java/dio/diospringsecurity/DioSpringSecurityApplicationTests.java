eff494181cffe0b21914eb869eddd1a165290535
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-28 19:27:14 -0300

    initial commit

---
+++ Spring-Framework/dio-spring-security/src/test/java/dio/diospringsecurity/DioSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.diospringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
