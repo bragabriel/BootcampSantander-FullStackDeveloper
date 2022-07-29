59eb5cec7f59fd3f91c17e88c1fcd20f43be24bd
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-29 20:12:03 -0300

    Update SingletonLazy.java

--- Spring-Framework/desafio-de-projeto/lab-padroes-projeto-java/src/praticando/java/SingletonLazy.java
+++ Spring-Framework/desafio-de-projeto/lab-padroes-projeto-java/src/praticando/java/SingletonLazy.java
@@ -5,5 +5,23 @@ package praticando.java;
   - não disponibiliza a instância para o usuário no primeiro momento
 */
 public class SingletonLazy {
+	
+	private static SingletonLazy instancia;
+	//Criando instância dele mesmo (Singleton)
 
+	
+	//método privado para garantir que ninguém vai instanciar ele fora da classe
+	private SingletonLazy() {
+		super();
+	}
+	
+	//garantindo que a instância vai ser exposta para quem for chamar
+	public static SingletonLazy getInstancia() {
+		//		tipo do retorno
+		
+		if(instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
 }
