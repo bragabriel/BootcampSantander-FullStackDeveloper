b174e302099cbf20e38f9edd4ce9e7b4e338ab68
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-29 20:23:05 -0300

    add: Singleton Lazy Holder

---
+++ Spring-Framework/desafio-de-projeto/lab-padroes-projeto-java/src/praticando/java/SingletonLazyHolder.java
@@ -0,0 +1,22 @@
+package praticando.java;
+
+/*
+  Singleton Lazy Holder.
+*/
+public class SingletonLazyHolder {
+	
+	private static class InstanceHolder{
+		//Encapsulando a instancia em uma classe interna estatica (Holder)
+		public static SingletonLazyHolder instancia = new SingletonLazyHolder();	
+	}
+	
+	//método privado para garantir que ninguém vai instanciar ele fora da classe
+	private SingletonLazyHolder() {
+		super();
+	}
+	
+	//garantindo que a instância vai ser exposta para quem for chamar
+	public static SingletonLazyHolder getInstancia() {
+		return InstanceHolder.instancia;
+	}
+}
