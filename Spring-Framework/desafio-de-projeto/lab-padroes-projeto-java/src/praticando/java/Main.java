d503b66d882e9645c8f1f05e44ea7bc3c7708ed4
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-29 20:26:38 -0300

    Update Main.java

--- Spring-Framework/desafio-de-projeto/lab-padroes-projeto-java/src/praticando/java/Main.java
+++ Spring-Framework/desafio-de-projeto/lab-padroes-projeto-java/src/praticando/java/Main.java
@@ -6,6 +6,17 @@ public class Main {
 		
 		/* Testes relacionados ao Design Pattern Singleton */
 		
+		/*
+		 Padrões de projeto:
+		 - Criacionais: ... Singleton;
+		 
+		 Singleton: O Padrão Singleton tem como definição garantir
+		  que uma classe tenha apenas uma instância de si mesma
+		  e que forneça um ponto global de acesso a ela.
+		  Ou seja, uma classe gerencia a própria instância dela
+		  além de evitar que qualquer outra classe crie uma instância dela.
+		 */
+		
 		SingletonLazy lazy = SingletonLazy.getInstancia();
 		System.out.println(lazy);
 		lazy = SingletonLazy.getInstancia();
