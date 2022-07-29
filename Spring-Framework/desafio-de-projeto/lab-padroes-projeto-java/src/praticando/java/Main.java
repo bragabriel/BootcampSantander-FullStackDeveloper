953504f334d8f9ba7d9c43940a9f2916a396162f
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-29 20:23:07 -0300

    Create Main.java

---
+++ Spring-Framework/desafio-de-projeto/lab-padroes-projeto-java/src/praticando/java/Main.java
@@ -0,0 +1,25 @@
+package praticando.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/* Testes relacionados ao Design Pattern Singleton */
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+	}
+
+}
