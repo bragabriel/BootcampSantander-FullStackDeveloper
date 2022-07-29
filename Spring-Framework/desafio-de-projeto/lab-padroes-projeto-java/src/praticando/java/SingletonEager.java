aed1e57ff7665c24801a1a3b9d5b36774e2d88bf
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-29 20:12:11 -0300

    add: Singleton Eager

---
+++ Spring-Framework/desafio-de-projeto/lab-padroes-projeto-java/src/praticando/java/SingletonEager.java
@@ -0,0 +1,24 @@
+package praticando.java;
+
+/*
+  Singleton 'apressado'.
+  - no momento em que a classe for acionada, ela vai será retornada
+*/
+public class SingletonEager {
+	
+	private static SingletonEager instancia;
+	//Criando instância dele mesmo (Singleton)
+
+	
+	//método privado para garantir que ninguém vai instanciar ele fora da classe
+	private SingletonEager() {
+		super();
+	}
+	
+	//garantindo que a instância vai ser exposta para quem for chamar
+	public static SingletonEager getInstancia() {
+		//		tipo do retorno
+		
+		return instancia;
+	}
+}
